95cf443366bf3782578f887a6c490cf634abc674
miishrak9 <miishrak9@gmail.com>
2018-12-01 20:05:56 +0800

    Construct Driver page 01

--- app/src/main/java/my/edu/um/fsktm/driveractivity/MainActivity.java
+++ app/src/main/java/my/edu/um/fsktm/driveractivity/MainActivity.java
@@ -1,13 +1,22 @@
 package my.edu.um.fsktm.driveractivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActionBar actionBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        actionBar=getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FB8C00")));
+        actionBar.setTitle("Driver");
+
     }
 }
