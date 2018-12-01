a16e24abe7d7df71e3aba1f9f311f78056e75c0b
miishrak9 <miishrak9@gmail.com>
2018-12-02 00:31:05 +0800

    Construct Edit Car (Adding toolbar) into activity

--- app/src/main/java/my/edu/um/fsktm/driveractivity/MainActivity.java
+++ app/src/main/java/my/edu/um/fsktm/driveractivity/MainActivity.java
@@ -5,18 +5,34 @@ import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActionBar actionBar;
+   // private ActionBar actionBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        actionBar=getSupportActionBar();
-        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FB8C00")));
-        actionBar.setTitle("Driver");
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.TB);
+        setSupportActionBar(myToolbar);
+
+    }
+
+    @Override
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true ;
+
+        //actionBar=getSupportActionBar();
+        //actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FB8C00")));
+        //actionBar.setTitle("Driver");
 
     }
 }
---
+++ app/src/main/java/my/edu/um/fsktm/driveractivity/feedback.java
@@ -0,0 +1,16 @@
+package my.edu.um.fsktm.driveractivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class feedback extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+
+
+
+    }
+}
