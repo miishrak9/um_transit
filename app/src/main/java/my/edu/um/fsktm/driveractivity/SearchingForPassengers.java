0c65513a351b01a0d5f3cd3dba9aee630ad31612
miishrak9 <miishrak9@gmail.com>
2018-12-08 20:54:00 +0800

    Loading screen for 'Looking for Passengers'

--- app/src/main/java/my/edu/um/fsktm/driveractivity/SearchingForPassengers.java
+++ app/src/main/java/my/edu/um/fsktm/driveractivity/SearchingForPassengers.java
@@ -1,10 +1,16 @@
 package my.edu.um.fsktm.driveractivity;
 
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toolbar;
 
 public class SearchingForPassengers extends AppCompatActivity {
+
+    AnimationDrawable animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +18,20 @@ public class SearchingForPassengers extends AppCompatActivity {
         setContentView(R.layout.activity_searching_for_passengers);
 
 
+        ImageView loading = (ImageView)findViewById(R.id.imageView);
+        animation = (AnimationDrawable)loading.getDrawable();
 
     }
+
+
+    public void start(View v){
+
+        animation.start();
+    }
+
+    public void stop(View v){
+
+        animation.stop();
+    }
 }
+
